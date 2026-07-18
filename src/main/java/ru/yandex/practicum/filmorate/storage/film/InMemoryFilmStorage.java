e4f3bc0bd1fcaeec4475f574df0ea7519e50e65c
pischulinaov <opischulina@mail.ru>
2026-07-19 01:04:37 +0400

    Финальное задание 11 спринта

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class InMemoryFilmStorage implements FilmStorage{
+public class InMemoryFilmStorage implements FilmStorage {
     // Хранилище фильмов: ключ — ID фильма, значение — объект Film
     private final Map<Long, Film> films = new HashMap<>();
 
