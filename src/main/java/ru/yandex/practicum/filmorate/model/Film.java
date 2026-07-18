b3ad506800878cdd89e2437b67449ef27b4f11a6
pischulinaov <opischulina@mail.ru>
2026-07-19 00:50:44 +0400

    Финальное задание 11 спринта

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -12,12 +12,11 @@ import java.util.Set;
  */
 @Data
 @EqualsAndHashCode
-//@Builder
 public class Film {
     Long id; //уникальный идентификатор,
     String name; //название
     String description; // описание
     LocalDate releaseDate; // дата релиза
     int duration; // продолжительность фильма
-    Set<Long> likes = new HashSet<>();;
+    Set<Long> likes = new HashSet<>();
 }
