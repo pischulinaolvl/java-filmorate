791edc0a2b20ccf17a7012e811a638fb5b724e54
pischulinaov <opischulina@mail.ru>
2026-09-21 13:44:27 +0400

    Финальное задание 12 спринта

--- src/main/java/ru/yandex/practicum/filmorate/storage/friend/FriendStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friend/FriendStorage.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.filmorate.storage.friend;
 
 import java.util.Map;
-import java.util.Set;
 
 public interface FriendStorage {
     void createFriendship(long user1Id, long user2Id, String status);
+
     void updateFriendship(long user1Id, long user2Id, String status);
 
     Map<Long, String> getFriends(long userId, boolean forSecondFriend);
