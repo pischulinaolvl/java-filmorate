9264aa5302d3cc3a641e574d9e966b8684e04322
pischulinaov <opischulina@mail.ru>
2026-09-21 13:46:04 +0400

    Финальное задание 12 спринта

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,14 +1,8 @@
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
 @AutoConfigureTestDatabase
@@ -16,15 +10,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FilmoRateApplicationTests {
     private final UserDbStorage userStorage;
 
-    /*@Test
-    public void testFindUserById() {
-
-        Optional<User> userOptional = userStorage.findUserById(1L);
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
-    }*/
 }
