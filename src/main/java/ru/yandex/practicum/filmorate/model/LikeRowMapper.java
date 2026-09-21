791d5d8a5f75c54c18726a33a5009545c013769f
pischulinaov <opischulina@mail.ru>
2026-09-21 13:33:26 +0400

    Финальное задание 12 спринта

--- src/main/java/ru/yandex/practicum/filmorate/model/LikeRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/LikeRowMapper.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class LikeRowMapper implements RowMapper<Like>{
+public class LikeRowMapper implements RowMapper<Like> {
     @Override
     public Like mapRow(ResultSet rs, int rowNum) throws SQLException {
         Like like = new Like();
--- src/main/java/ru/yandex/practicum/filmorate/model/MpaaRatingRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaaRatingRowMapper.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class MpaaRatingRowMapper implements RowMapper<MpaaRating>{
+public class MpaaRatingRowMapper implements RowMapper<MpaaRating> {
     @Override
     public MpaaRating mapRow(ResultSet rs, int rowNum) throws SQLException {
         MpaaRating mpaaRating = new MpaaRating();
